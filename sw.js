737e6001864aed27daa333d31e9eb84940a93325
James Priest <james-priest@outlook.com>
2018-06-25 00:32:06 -0700

    feat: Bring A11y metric to 95 with better link contrast colors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'restaurant-static-039';
+const staticCacheName = 'restaurant-static-041';
 
 // list of assets to cache on install
 // cache each restaurant detail page as well
